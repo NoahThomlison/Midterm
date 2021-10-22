fee1d7ec2625a3f075e36c04af314d596197abcf
Lendl Peralta <janlendl@gmail.com>
2021-10-22 18:15:18 +0000

    Create seeds for categories table

---
+++ db/seeds/03_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (type) VALUES ('Movies and TV');
+INSERT INTO categories (type) VALUES ('Restaurants and Cafes');
+INSERT INTO categories (type) VALUES ('Books and Magazines');
+INSERT INTO categories (type) VALUES ('Items and Produce');
+INSERT INTO categories (type) VALUES ('Uncategorized');
