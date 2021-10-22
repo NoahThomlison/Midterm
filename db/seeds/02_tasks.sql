5033d4411e7ca5dcd5202f899b771c5b0152d33f
Lendl Peralta <janlendl@gmail.com>
2021-10-22 18:14:56 +0000

    Create seed for tasks table

---
+++ db/seeds/02_tasks.sql
@@ -0,0 +1,21 @@
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Watch movie', 'Watch batman movie', 1, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Dinner with Laura', 'Meet at Earls', 2, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Check out the latest Harry Potter book', 'Indigo at Market Mall have some', 3, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Order dinner at an Italian restaurant', 'Order pasta and chicken parmesan', 2, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES
+('Go to bestbuy', 'Look for GoPro battery', 4, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES
+('Shop for a new shoes', 'Check out the new Jordan shoes', 4, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Season 40 of Survivor', 'Make sure to set the recording', 1, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Order coffee for the boss this Friday', 'He wants 2 shots of esperesso', 2, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES
+('New book by RR Tolkien', 'Release is on Thursday', 3, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Checkout newly opened Chinese restaurant', 'Dont forget to remind Noah to join', 2, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Netflix Witcher Season 2', 'Release is on Saturday', 2, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES ('Grocery run', 'Milk, eggs, bacon', 4, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES
+('John Wick Chapter 4', 'New trailer drop', 5, 1, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES
+('Bubble Tea', 'Dont forget to remind Noah to join to', 5, 2, FALSE, null, FALSE);
+INSERT INTO tasks (title, description, category_id, user_id, set_reminder, reminder_date, completion_status) VALUES
+('Diablo 4 release', 'Dont forget to pre-order, 5, 1, FALSE, null, FALSE);
