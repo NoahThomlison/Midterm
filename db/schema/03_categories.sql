8e9b4dcf7f0c5f6fffd653649e69254790d976ae
noahthomlison <noahthomlison@gmail.com>
2021-10-22 01:15:04 +0000

    corrects typos

--- db/schema/03_categories.sql
+++ db/schema/03_categories.sql
@@ -1,7 +1,7 @@
 -- Drop and recreate catagories table (Example)
 
-DROP TABLE IF EXISTS catagories CASCADE;
-CREATE TABLE catagories (
+DROP TABLE IF EXISTS categories CASCADE;
+CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
   type VARCHAR(255) NOT NULL
 );
