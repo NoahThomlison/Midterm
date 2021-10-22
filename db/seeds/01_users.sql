12cc646b1c0555e413d02c57c39b12ebe11a752c
Lendl Peralta <janlendl@gmail.com>
2021-10-22 18:14:34 +0000

    Remove comment

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
--- Users table seeds here (Example)
+
 INSERT INTO users (name, email, password) VALUES ('Alice Wonderland', 'yellowbrickroad@yahoo.ca', 'password');
 INSERT INTO users (name, email, password) VALUES ('Kira Daily', 'myemail@google.ca', 'password');
