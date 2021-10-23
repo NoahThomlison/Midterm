7d47ee2f0688a174f0a6ab13a266e8a33b1b5314
noahthomlison <noahthomlison@gmail.com>
2021-10-23 21:22:50 +0000

    updates sersver with testing code

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(morgan("dev"));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json())
 app.use(
   "/styles",
   sassMiddleware({
@@ -36,12 +36,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const tasksRoutes = require("./routes/tasks");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/tasks", tasksRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
@@ -51,6 +51,17 @@ app.use("/api/widgets", widgetsRoutes(db));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/tasks", (req, res) => {
+  console.log('hello')
+  res.render("index");
+
+});
+
+// app.post("/tasks/new", (req, res) => {
+//   console.log(req.body)
+//   console.log(tasksRoutes(req.body))
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
