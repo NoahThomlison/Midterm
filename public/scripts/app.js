9bf29a65c55224e27c38a8e89b7110fa7e019707
noahthomlison <noahthomlison@gmail.com>
2021-10-23 18:15:16 +0000

    adds eventlistener basics for the submit form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,9 @@
 // Client facing scripts here
+$(document).ready(function(){
+console.log('running')
+
+  $('#newTaskForm').submit(function(event){
+    event.preventDefault();
+    const newTask = $('#task-text').val()
+  })
+})
