94911559f4454d3e9df7acffabd19d95434ff136
Lendl Peralta <janlendl@gmail.com>
2021-10-23 18:32:06 -0600

    Create task routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,54 +9,135 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+
+
+
+
   // /api/tasks/
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM tasks`;
-    console.log("/api/tasks/")
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+  // router.get('/', (req, res) => {
+  //   let query = `SELECT * FROM tasks`;
+  //   console.log('/api/tasks/')
+  //   console.log(query);
+  //   db.query(query)
+  //     .then(data => {
+  //       const widgets = data.rows;
+  //       res.json({ widgets });
+  //     })
+  //     .catch(err => {
+  //       res
+  //         .status(500)
+  //         .json({ error: err.message });
+  //     });
+  // });
+
+  // /api/tasks/
+// router.post('/', (req, res) => {
+//   console.log('/api/tasks/')
+//   console.log(req.body);
+//   // res.json({status: 'hello'})
+//   res.send('hi max')
+// })
+
+// /api/tasks/new
+// router.post('/new', (req, res) => {
+//   console.log('ding')
+//   console.log(req.body)
+//   // console.log(res)
+//   res.json({status: 'hello'})
+// })
+
+
+
+  // create a new task
+  router.post('/new', (req, res) => {
+    const newTaskQuery = `INSERT INTO tasks (title, description, user_id) VALUES ($1. $2, $3)`;
+    const queryParams = [title, description, user_id];
+
+    db.query(newTaskQuery, queryParams)
+      .then((res) => {
+        const newTask = res.rows[0];
+        res.send(newTask);
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) =>{
+        console.log('Error', err.message);
       });
   });
 
-  // /api/tasks/
-router.post('/', (req, res) => {
-  console.log('/api/tasks/')
-  console.log(req.body);
-  // res.json({status: 'hello'})
-  res.send('hi max')
-})
 
-// /api/tasks/new
-router.post('/new', (req, res) => {
-  console.log('ding')
-  console.log(req.body)
-  // console.log(res)
-  res.json({status: 'hello'})
-})
+  // view all tasks
+  router.get('/', (req, res) => {
+    const queryString = `SELECT * FROM tasks WHERE user_id = $1`
+    const values = [1];
+    db.query(queryString, values)
+      .then ((res) => {
+        const data = res.rows;
+        res.json({ data });
+      })
+      .catch(() => {
+        console.log('Error', err.message);
+        res.send(err);
+      });
+  });
 
-  // /api/tasks/new
-router.get("/new", (req, res) => {
-  let query = `SELECT * FROM tasks`;
-  console.log(query);
-  db.query(query)
-    .then(data => {
-      const widgets = data.rows;
-      res.json({ widgets });
+  // view all tasks per category
+ router.get('/category/:category_id', (req, res) => {
+   let queryString = `SELECT * FROM categories WHERE category_id = $1 AND user_id = $2`;
+   let values = [1, 1];
+   db.query(queryString, values)
+    .then((res) => {
+      const data = res.rows;
+      res.json( { data });
     })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+    .catch((err) => {
+      console.log('Error', err.message);
+      res.send(err);
     });
-});
+ });
+
+  // delete a specific task
+  router.post('/:tasksId/delete', (req, res) => {
+  let deleteTaskQuery = `DELETE FROM tasks WHERE id = $1`;
+  const values = [taskID];
+
+  db.query(deleteTaskQuery, values)
+    .then(()=> {
+      res.status(200);
+      console.log('Sucessfully deleted task');
+    })
+    .catch((err) => {
+      console.log('Error', err.message);
+    });
+  });
+
+    // update a task
+  router.post('/:tasksId/update', (req, res) => {
+    let updateTaskQuery = `UPDATE tasks SET title = $1 WHERE id = $2 RETURNING *`;
+    let values = ['This is a TEST title change', 1];
+
+    db.query(updateTaskQuery, values)
+      .then((res) => {
+        const updatedTask = res.rows[0];
+        res.send(updatedTask);
+      })
+      .catch((err) => {
+        console.log('Error', err.message);
+      });
+  });
+
+    // mark task as complete
+  router.post('/:taskId', (req, res) => {
+    const markCompleteQuery = `UPDATE tasks SET completion_status = TRUE WHERE id = $1`;
+    const queryParams = [taskID];
+
+    db.query(markCompleteQuery, queryParams)
+      .then((res) => {
+        const taskComplete = res.rows[0];
+        res.send(taskComplete);
+      })
+      .catch((err) => {
+        console.log('Error', err.message);
+      });
+  });
 
   return router;
 };
