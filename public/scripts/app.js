4789d6e250056a82f5e3c07de5009ed60036d0ec
noahthomlison <noahthomlison@gmail.com>
2021-10-24 01:01:03 +0000

    adds pathing for various word searchs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,29 @@
 // Client facing scripts here
 $(document).ready(function(){
-console.log('running2')
 
   $('#newTaskForm').submit(function(event){
     event.preventDefault();
-    form = $(this)
     const newTask = $('#task-text').val()
-    console.log(form)
 
+
+    console.log(newTask);
+    let splitString = newTask.split(' ')
+    console.log(splitString)
 // // sorting functions
 // if single word call funtionX
 // if multiword call functionY
-    console.log(typeof(newTask))
-    console.log(newTask)
-    // $.post('api/tasks/new', {data: newTask})
-    //   .then(data => {
-    //     $('.food').append(`<li>${data.status}</li>`)
-    //     console.log(data);
-    //   })
-    $.post('api/tasks/', {data: newTask})
-      .then(data => {
-        console.log(`data: ${data}`);
-        console.log('ding');
-      })
 
-    // $.post('api/tasks/', {data: newTask})
+    if(splitString.length = 1){
+      category = singleWordMatcher(splitString)
+    }
+
+    if(splitString.length > 1){
+      category = multiWordMatcher(splitString)
+    }
+
+    $.post('api/tasks/new', {task: newTask,category: 1})
+    $(`.foodList`).append(`<li>${newTask}</li>`)
+    $('#task-text').val('')
+
   })
 })
