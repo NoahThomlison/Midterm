b765bcaac150d9da72f585bc4b8057f4c9608a14
noahthomlison <noahthomlison@gmail.com>
2021-10-25 01:58:55 +0000

    adds working functionality for app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,46 +1,39 @@
 // Client facing scripts here
+// import fetch from "node-fetch";
 
 $(document).ready(function(){
-
-  const newTaskListItem =
-      `<li class='toDoListItem'>
-        <div class='toDoRightSide'>
-          <button class='listButton'>
-          <i class="fas fa-check-circle"></i>
-          </button>
-          <button class='listButton'>
-            <i class="fas fa-minus-circle"></i>
-          </button>
-          <div class="task">test
-          </div>
-        </div>
-        <button class='listButton'>
-          <i class="fas fa-filter"></i>
-        </button>
-      </li>`
 
   $('#newTaskForm').submit(function(event){
     event.preventDefault();
     const newTask = $('#task-text').val()
-    console.log(newTask);
     let splitString = newTask.split(' ')
-    console.log(splitString)
-    // // sorting functions
-    // if single word call funtionX
-    // if multiword call functionY
-    // (wikiSearch(newTask))
-    // console.log(wikiSearch(newTask))
+    // console.log(splitString)
 
-    // if(splitString.length = 1){
-    //   category = singleWordMatcher(splitString)
-    // }
-
-    // if(splitString.length > 1){
-    //   category = multiWordMatcher(splitString)
-    // }
-
-    $.post('api/tasks/new', {task: newTask,category: 1})
-    $(`.foodList`).append(newTaskListItem)
-    $('#task-text').val('')
+    if(splitString.length = 1){
+      console.log('ding')
+      $.post('api/keywords', {keyword: newTask})
+      .then((category) => {
+        console.log(category)
+        $.post('api/tasks/new', {task: newTask, category: category})
+        $(`#${category}`).append(
+          `<li class='toDoListItem'>
+            <div class='toDoRightSide'>
+              <button class='listButton'>
+              <i class="fas fa-check-circle"></i>
+              </button>
+              <button class='listButton'>
+                <i class="fas fa-minus-circle"></i>
+              </button>
+              <div class="task">${newTask}
+              </div>
+            </div>
+            <button class='listButton'>
+              <i class="fas fa-filter"></i>
+            </button>
+          </li>`)
+        $('#task-text').val('')
+        }
+      )
+    }
   })
 })
