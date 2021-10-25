71e0cdba0c448160eb8123caf6b1d60db5f9a7c0
noahthomlison <noahthomlison@gmail.com>
2021-10-25 02:00:49 +0000

    adds keywords.js which pings external api and checks against keywords database to determine catagory

---
+++ routes/keywords.js
@@ -0,0 +1,84 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+const fetch = require('cross-fetch');
+const { response } = require('express');
+
+module.exports = (db) => {
+  router.post("/", (req, res) => {
+    let keyword = req.body.keyword
+    let count = 0
+
+    const wikiPromise =
+      wikiSearch(keyword).then((wikiData) => {return (wikiData)})
+
+    const dbPromise = db.query(`SELECT * FROM keywords;`)
+    .then(data => {
+      const dbKeywords = data.rows;
+      return(dbKeywords)
+    })
+    .catch(err => {
+      res.status(500)
+      res.json({ error: err.message });
+      });
+
+    return Promise.all([wikiPromise, dbPromise]).then((values) => {
+      let wikiValues = values[0]
+      let dbPromise = values[1]
+      let categories = {}
+
+      let wikiStringSnippet = ''
+
+      for (const wiki of wikiValues) {
+        wikiStringSnippet = wikiStringSnippet + ' ' + wiki.snippet
+      }
+
+      for (const db of dbPromise) {
+        if(wikiStringSnippet.includes(db.keyword)){
+          console.log(db.keyword)
+          categories[db.category_id] = (categories[db.category_id]+1) || 1 ;
+        }
+      }
+      let correctCatagory = Object.keys(categories).reduce((a, b) => categories[a] > categories[b] ? a : b);
+
+      console.log(categories)
+      console.log(correctCatagory)
+      res.json(correctCatagory)
+      });
+  });
+  return router;
+};
+
+function wikiSearch (keyword) {
+
+  let url = "https://en.wikipedia.org/w/api.php";
+  let params = {
+    action: "query",
+    prop: "extracts",
+    list: "search",
+    srsearch: keyword,
+    srlimit: 1000,
+    format: "json",
+    exchars: 1200
+    };
+
+  url = url + "?origin=*";
+
+  Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+  console.log(url)
+
+  let data = fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      return(data.query.search)})
+    .catch(function(error){console.log(error);});
+
+  return data
+}
