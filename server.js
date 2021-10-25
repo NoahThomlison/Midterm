9f083bb4b2a594a74af0ec7124144017cc478fe5
noahthomlison <noahthomlison@gmail.com>
2021-10-25 01:58:15 +0000

    adds routings for keywords

--- server.js
+++ server.js
@@ -37,11 +37,14 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const tasksRoutes = require("./routes/tasks");
+const keywordsRoutes = require("./routes/keywords");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/tasks", tasksRoutes(db));
+app.use("/api/keywords", keywordsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
