2989c8f7fbfd56c9c9cc9f564f0310b9adcc0ed1
noahthomlison <noahthomlison@gmail.com>
2021-10-28 01:10:54 +0000

    adds new function which adds eventlisteners to mark a task as complete

---
+++ public/scripts/complete.js
@@ -0,0 +1,15 @@
+// Event Listener for the delete button
+
+completeButton = () => {
+  $('.listButtonComplete').on("click",(function(event){
+    event.preventDefault();
+    const splitID = (this.id).split('-')
+    const id = splitID[0]
+    $.post(`/api/tasks/${id}`, {tasksId: id})
+     .then((category) => {
+       $(`#${id}-Text`).css("text-decoration", "line-through")
+    })
+  }))
+}
+
+module.exports = completeButton
