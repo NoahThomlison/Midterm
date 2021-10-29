6ecab75fb83110edff931f83f75992fb9a2d1742
Lendl Peralta <janlendl@gmail.com>
2021-10-29 03:19:18 +0000

    Fix delete jquery

--- public/scripts/delete.js
+++ public/scripts/delete.js
@@ -1,19 +1,22 @@
 // Event Listener for the delete button
 
-deleteButton = () => {
-  $('.listButtonDelete').on("click",(function(event){
-    event.preventDefault();
-    console.log('ding')
-    const splitID = (this.id).split('-')
-    const id = splitID[0]
-    const parentListItem = this.closest('li')
-    console.log(parentListItem)
-    console.log(id)
+$(document).ready(function(){
+  $('.listButtonDelete').on("click", function(event) {
+      event.preventDefault();
+      console.log('ding')
+      const splitID = (this.id).split('-')
+      const id = splitID[0]
+      const parentListItem = this.closest('li')
+      console.log(parentListItem)
+      console.log(id)
 
-    $.post(`/api/tasks/${id}/delete`, {tasksId: id})
-     .then((category) => {
-        console.log(parentListItem)
-        parentListItem.remove()
-    })
-  }))
-}
+      $.post(`/api/tasks/${id}/delete`, {tasksId: id})
+      .then((category) => {
+          console.log(parentListItem)
+          parentListItem.remove()
+      })
+      .catch((err) => {
+        console.log('Error: ', err.message);
+      });
+  });
+});
