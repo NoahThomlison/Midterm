52f4da5d4dbd5ad8cc31d646cb388152015e221e
Lendl Peralta <janlendl@gmail.com>
2021-10-29 03:18:38 +0000

    Remove previous code I added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,42 +33,4 @@ $(document).ready(function(){
       })
     })
   })
-
-  $('.listButtonDelete').on("click", function(deleteButton) {
-    deleteButton = () => {
-      event.preventDefault();
-      console.log('ding')
-      const splitID = (this.id).split('-')
-      const id = splitID[0]
-      const parentListItem = this.closest('li')
-      console.log(parentListItem)
-      console.log(id)
-
-      $.post(`/api/tasks/${id}/delete`, {tasksId: id})
-       .then((category) => {
-          console.log(parentListItem)
-          parentListItem.remove()
-      })
-      .catch((err) => {
-        console.log('Error: ', err.message);
-      });
-    }
-  });
-
-
-  $('.listButtonComplete').on("click", function(completeButton) {
-    completeButton = () => {
-      event.preventDefault();
-      const splitID = (this.id).split('-')
-      const id = splitID[0]
-
-      $.post(`/api/tasks/${id}`, {tasksId: id})
-      .then((category) => {
-        $(`#${id}-Text`).css("text-decoration", "line-through")
-      })
-      .catch((err) => {
-        console.log('Error: ', err.message);
-      });
-    }
-  });
 });
